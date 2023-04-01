717cb45632803150f58d0058b2f42b21c5fd49a1
NickMC05 <n1ch0l45wk@gmail.com>
2023-04-01 17:34:42 +0800

    updated menu.cpp = added credits screen and resmooth the experience

--- MenuScreen/menu.cpp
+++ MenuScreen/menu.cpp
@@ -10,6 +10,7 @@
 // Declaring screen function
 void loading_screen();
 void home_screen(int input, int select, int max_choice);
+void credits_screen();
 
 
 
@@ -17,17 +18,15 @@ void home_screen(int input, int select, int max_choice);
 int main() {
 
     // Screen setup
-    system("clear");
-    erase();
+    system("clear"); erase();
     setlocale(LC_ALL, "en_US.UTF-8");
-    initscr();
-    cbreak();
-    noecho();
+    initscr(); cbreak(); noecho();
     keypad(stdscr, TRUE);
 
     // Menu variables
     int input;
     int select = 1;
+    int current_page = 1; // home_screen, play, options, credits, exit
     int max_choice = 4; // adjustable depends on how many choices
 
     // Screen check to make sure the player is on optimized screen size
@@ -46,35 +45,54 @@ int main() {
         }
     }
 
-    // Screen (first start-up)
     loading_screen();
-    home_screen(input, select, max_choice);
 
     // Menu Screen
-    
-    // Home screen choosing
-    while ((input = getch()) != KEY_F(1)) {
-        // Receiving input
-        if (input == 97 || input == 119 || input == 259 || input == 260) { // down
-            select -= 1;
-        } else if (input == 100 || input == 115 || input == 258 || input == 261) { // up
-            select += 1;
-        } else if (input == 10) { // enter
+    while (true) {
+
+        if (current_page == 1) {
+            // Home screen choosing
+            home_screen(input, select, max_choice);
+            while ((input = getch()) != KEY_F(1)) {
+                // Receiving input
+                if (input == 97 || input == 119 || input == 259 || input == 260) { // down
+                    select -= 1;
+                } else if (input == 100 || input == 115 || input == 258 || input == 261) { // up
+                    select += 1;
+                } else if (input == 10) { // enter
+                    if (select == 1) {
+                        current_page = 1; // change to play
+                    } else if (select == 2) {
+                        current_page = 1; // change to options
+                    } else if (select == 3) {
+                        current_page = 4; // change to credits
+                    } else if (select == 4) {
+                        current_page = 5;
+                    } 
+                    erase();
+                    break;
+                }
+                if (select <= 0) {
+                    select = max_choice;
+                } else if (select > max_choice) {
+                    select = 1;
+                }
+
+                // Screen
+                home_screen(input, select, max_choice);
+            }
+        } else if (current_page == 4) {
+            credits_screen();
+            while ((input = getch()) != KEY_F(1)) {
+                break;
+            }
+            current_page = 1;
+        } else if (current_page == 5) {
             erase();
-            printw("ENTER");
-            move(0,0); refresh();
-            usleep(1000000);
             break;
         }
-        if (select <= 0) {
-            select = max_choice;
-        } else if (select > max_choice) {
-            select = 1;
-        }
-
-        // Screen
-        home_screen(input, select, max_choice);
     }
+    // End
     endwin();
 }
 
@@ -163,12 +181,12 @@ void home_screen(int input, int select, int max_choice) {
             mvprintw(34,30,"                         /____/   ");
             break;
         case 2:
-            mvprintw(37,30,"              ____        __                    ");
-            mvprintw(38,30,"____         / __ \\____  / /_(_)___  ____  _____");
-            mvprintw(39,30,"\\ \\ \\       / / / / __ \\/ __/ / __ \\/ __ \\/ ___/");
-            mvprintw(40,30," \\ \\ \\     / /_/ / /_/ / /_/ / /_/ / / / (__  ) ");
-            mvprintw(41,30," / / /     \\____/ .___/\\__/_/\\____/_/ /_/____/  ");
-            mvprintw(42,30,"/_/_/          /_/                              ");
+            mvprintw(37,30,"____          ____        __                    ");
+            mvprintw(38,30,"\\ \\ \\        / __ \\____  / /_(_)___  ____  _____");
+            mvprintw(39,30," \\ \\ \\      / / / / __ \\/ __/ / __ \\/ __ \\/ ___/");
+            mvprintw(40,30," / / /     / /_/ / /_/ / /_/ / /_/ / / / (__  ) ");
+            mvprintw(41,30,"/_/_/      \\____/ .___/\\__/_/\\____/_/ /_/____/  ");
+            mvprintw(42,30,"               /_/                              ");
             break;
         case 3:
             mvprintw(45,30,"____          ______              __  __"      );
@@ -186,5 +204,64 @@ void home_screen(int input, int select, int max_choice) {
             break;
     }
     
+    move(0,0); refresh();
+}
+
+// Credits screen
+void credits_screen() {
+    erase();
+
+    mvprintw(2,20,"______________________________________  ._______________________");
+    mvprintw(3,20,"\\_   ___ \\______   \\_   _____/\\______ \\ |   \\__    ___/   _____/");
+    mvprintw(4,20,"/    \\  \\/|       _/|    __)_  |    |  \\|   | |    |  \\_____  \\ ");
+    mvprintw(5,20,"\\     \\___|    |   \\|        \\ |    `   \\   | |    |  /        \\");
+    mvprintw(6,20," \\______  /____|_  /_______  / |______  /___| |____| /_______  /");
+    mvprintw(7,20,"        \\/       \\/        \\/         \\/                     \\/ ");
+
+    mvprintw(12,35," _____ _                              _____         _    _ _             ");
+    mvprintw(13,35,"/  __ \\ |                            |_   _|       | |  | (_)            ");
+    mvprintw(14,35,"| /  \\/ |__   ___ _   _ _ __   __ _    | |___ ____ | |  | |_ _ __   __ _ ");
+    mvprintw(15,35,"| |   | '_ \\ / _ \\ | | | '_ \\ / _` |   | / __|_  / | |/\\| | | '_ \\ / _` |");
+    mvprintw(16,35,"| \\__/\\ | | |  __/ |_| | | | | (_| |   | \\__ \\/ /  \\  /\\  / | | | | (_| |");
+    mvprintw(17,35," \\____/_| |_|\\___|\\__,_|_| |_|\\__, |   \\_/___/___|  \\/  \\/|_|_| |_|\\__, |");
+    mvprintw(18,35,"                               __/ |                                __/ |");
+    mvprintw(19,35,"                              |___/                                |___/ ");
+
+    mvprintw(22,35," _____              ______                            ");
+    mvprintw(23,35,"|  __ \\             | ___ \\                           ");
+    mvprintw(24,35,"| |  \\/_   _  ___   | |_/ /__ _ __   __ _ _   _ _   _ ");
+    mvprintw(25,35,"| | __| | | |/ _ \\  |  __/ _ \\ '_ \\ / _` | | | | | | |");
+    mvprintw(26,35,"| |_\\ \\ |_| | (_) | | | |  __/ | | | (_| | |_| | |_| |");
+    mvprintw(27,35," \\____/\\__,_|\\___/  \\_|  \\___|_| |_|\\__, |\\__, |\\__,_|");
+    mvprintw(28,35,"                                     __/ | __/ |      ");
+    mvprintw(29,35,"                                    |___/ |___/       ");
+
+    mvprintw(32,35," _   __                 _                             _   _ _      _           _             _    _ _ _                 ");
+    mvprintw(33,35,"| | / /                (_)                           | \\ | (_)    | |         | |           | |  | (_) |                ");
+    mvprintw(34,35,"| |/ / _   _ _ __ _ __  _  __ ___      ____ _ _ __   |  \\| |_  ___| |__   ___ | | __ _ ___  | |  | |_| |___  ___  _ __  ");
+    mvprintw(35,35,"|    \\| | | | '__| '_ \\| |/ _` \\ \\ /\\ / / _` | '_ \\  | . ` | |/ __| '_ \\ / _ \\| |/ _` / __| | |/\\| | | / __|/ _ \\| '_ \\ ");
+    mvprintw(36,35,"| |\\  \\ |_| | |  | | | | | (_| |\\ V  V / (_| | | | | | |\\  | | (__| | | | (_) | | (_| \\__ \\ \\  /\\  / | \\__ \\ (_) | | | |");
+    mvprintw(37,35,"\\_| \\_/\\__,_|_|  |_| |_|_|\\__,_| \\_/\\_/ \\__,_|_| |_| \\_| \\_/_|\\___|_| |_|\\___/|_|\\__,_|___/  \\/  \\/|_|_|___/\\___/|_| |_|");
+
+    mvprintw(40,35," _   _                               _ _              ______           _     _                         ");
+    mvprintw(41,35,"| \\ | |                             (_|_)             |  _  \\         (_)   | |                        ");
+    mvprintw(42,35,"|  \\| | __ _ _ __ ___  ___ _ __ __ _ _ _  __ ___   __ | | | |___  _ __ _  __| | ___ _ __ ___ _ __ ___  ");
+    mvprintw(43,35,"| . ` |/ _` | '_ ` _ \\/ __| '__/ _` | | |/ _` \\ \\ / / | | | / _ \\| '__| |/ _` |/ _ \\ '__/ _ \\ '_ ` _ \\ ");
+    mvprintw(44,35,"| |\\  | (_| | | | | | \\__ \\ | | (_| | | | (_| |\\ V /  | |/ / (_) | |  | | (_| |  __/ | |  __/ | | | | |");
+    mvprintw(45,35,"\\_| \\_/\\__,_|_| |_| |_|___/_|  \\__,_|_| |\\__,_| \\_/   |___/ \\___/|_|  | |\\__,_|\\___|_|  \\___|_| |_| |_|");
+    mvprintw(46,35,"                                     _/ |                            _/ |                              ");
+    mvprintw(47,35,"                                    |__/                            |__/                               ");
+    
+    mvprintw(50,35," _    _ _ _                   _               ______ _      _     _      ");
+    mvprintw(51,35,"| |  | (_) |                 | |              | ___ (_)    | |   (_)     ");
+    mvprintw(52,35,"| |  | |_| |_   _ _ __  _ __ | |_ ___  _ __   | |_/ /_  ___| |__  _  ___ ");
+    mvprintw(53,35,"| |/\\| | | | | | | '_ \\| '_ \\| __/ _ \\| '_ \\  |    /| |/ __| '_ \\| |/ _ \\");
+    mvprintw(54,35,"\\  /\\  / | | |_| | | | | | | | || (_) | | | | | |\\ \\| | (__| | | | |  __/");
+    mvprintw(55,35," \\/  \\/|_|_|\\__, |_| |_|_| |_|\\__\\___/|_| |_| \\_| \\_|_|\\___|_| |_|_|\\___|");
+    mvprintw(56,35,"             __/ |                                                       ");
+    mvprintw(57,35,"            |___/                                                        ");
+
+    mvprintw(59,180,"Press any key to return.");
+
     move(0,0); refresh();
 }

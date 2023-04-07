2e20614b3aa1422cfaa8f7885cbb8ccde1238627
jajashit <nsmvpaffbd@gmail.com>
2023-04-07 16:53:27 +0800

    background

--- Background.cpp
+++ Background.cpp
@@ -1,16 +1,12 @@
 #include "Background.h"
+#include "Sprite.h"
 #include "State.h"
 #include <ncurses.h>
+#include <fstream>
 
 void Background::draw()
 {
     vector<vector<int>> &graphic = State::graphic;
-    for (int y = 0; y < 25; y++)
-    {
-        for (int x = 0; x < 100; x++)
-        {
-
-            graphic[y][x] = 1;
-        }
-    }
+    sprite = new Sprite("background.txt");
+    sprite->draw(0, 0);
 }

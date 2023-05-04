3440bb8eccae9cf26023234b990952e949102761
JJ <jj@JJdeMacBook-Pro.local>
2023-05-04 12:00:28 +0800

    Car.cpp Car.h main.cpp documentation

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@ int main()
 {
     //Game object will be created from here to run the game
     //Game::run() will initialiaze the game and call update(), draw() function
+    // This is good practice for games and follows lot of paradigms from popular game engines
+
     Game game;
     game.run();
     return 0;

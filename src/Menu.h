946393759d069607b59684a3a9e9c3d817814e8b
NickMC05 <n1ch0l45wk@gmail.com>
2023-05-04 17:55:09 +0800

    Added options page on Menu

--- src/Menu.h
+++ src/Menu.h
@@ -8,6 +8,7 @@ public:
     int run();
     void loading_screen();
     void home_screen(int input, int select, int max_choice);
+    void options_screen(int select, bool choosing_mode_1, bool choosing_mode_2, int choice_1, int choice_2);
     void credits_screen();
 
 private:
@@ -15,6 +16,8 @@ private:
     int select = 1;
     int current_page = 1; // home_screen, play, options, credits, exit
     int max_choice = 4;   // adjustable depends on how many choices
+    int difficulty = 1;   // 1 easy, 2 medium, 3 hard
+    int car_color = 1;    // 1 red, 2 yellow, 3 cyan
 };
 
 #endif

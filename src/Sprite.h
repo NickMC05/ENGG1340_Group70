f08e2b563bd154bf065ac05776c0e34ef85585bc
JJ <jj@JJdeMacBook-Pro.local>
2023-05-04 12:07:07 +0800

    Document Sprite

--- src/Sprite.h
+++ src/Sprite.h
@@ -1,3 +1,6 @@
+// Sprite class will load txt file and read pixels
+// it can draw to the screen accordingly at any positon
+
 #ifndef SPRITE_H
 #define SPRITE_H
 
@@ -12,10 +15,13 @@ class State;
 class Sprite
 {
 public:
+    // path is location of sprite fiel (aka txt file)
     string path;
+    // init() will load the file and read the values and push it to vector<int> pixel
     void init();
+    // it won't print anything to screen but update state->graphics so later game will print for it
     void draw(int x, int y);
-
+    // We need state to access graphics vector
     State *state;
 
 private:
